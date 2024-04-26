64b769537b542c31f2e8f881a3fb02a2f0b446ac
Mika <mika@winter.lan>
2024-04-26 22:42:53 +0300

    My first ice app

---
+++ ice.py
@@ -0,0 +1,6 @@
+st.write('Loadin capacity of ice')
+st.write('Loading capacity (kg) = 5 * thickness (cm) ^2')
+
+thickness = st.slider('Thickness of ice (cm)', 0, 200, 10)
+load = 5 * thickness ** 2
+st.write('The load capacity of the ice is', load, 'kg')
